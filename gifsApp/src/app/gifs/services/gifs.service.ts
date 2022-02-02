d1b57348d46d0b35c561312752a68e628cc58515
unknown <angeljpa95@gmail.com>
2022-02-02 14:34:28 -0600

    02 - gifs app finished

--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,6 +1,8 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, GifsResponse } from '../interface/gifs.interface';
 
-/* providedIn - añadida en la v4 de angular, permite que los servicios pueden estar incluidos en el momento que se crea el bundled de la app.
+/* providedIn - Se utiliza en la parte de los servicios y esta fue añadida en la v4 de angular, permite que los servicios pueden estar incluidos en el momento que se crea el bundled de la app.
 
 De esta manera le indica a angular que no importa en que parte de la app se encuentre, este servicio será unico y de manera global en el root.
 
@@ -13,15 +15,50 @@ Así no tenemos que especificarlos en los providers. De hacerlo solo lo podremos
 })
 export class GifsService {
 
+  private apiKey: string = 'B2rEp8Z0ViSdlKZPyK4FD7BxHf1zaDqM';
   private _historial: string[] = [];
+
+
+
+  public apiGif: string = 'http://api.giphy.com/v1/gifs';
+  public resultados:Gif[] = [];
+
+  constructor(private http: HttpClient){
+
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+  }
+
 
   get historial(){
     return [...this._historial]
   }
 
-  buscarGifs(query:string){
-    this._historial.unshift(query);
+  async buscarGifs(query:string){
 
-    console.log(this._historial);
+    query = query.trim().toLocaleLowerCase();
+
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,9);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial))
+    }
+
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', query);
+
+    // console.log(params.toString());
+
+    this.http.get<GifsResponse>(`${this.apiGif}/search`, { params })
+      .subscribe((resp) => {
+        //console.log(resp.data)
+        this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados))
+      })
+
   }
 }
--- gifsApp/src/app/shared/sidebar/sidebar.component.ts
+++ gifsApp/src/app/shared/sidebar/sidebar.component.ts
@@ -15,5 +15,9 @@ export class SidebarComponent{
     return this.gifsService.historial;
   }
 
+  buscar(item: string ){
+    this.gifsService.buscarGifs(item);
+  }
+
 
 }
